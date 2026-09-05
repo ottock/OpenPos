41fa2760424e63ac6e0408b1bd0cd4cc4e400b74
ottock <ottock@outlook.com>
2026-09-05 11:00:04 -0300

    bumbumbum

--- backend/src/repository/postgres/query/configuracao/read_configuracao.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-	Id AS id,
-	DiretorioSalvamento AS diretorio_salvamento,
-	AtualizadoEm AS atualizado_em
-FROM cadpos.Configuracao
-ORDER BY Id DESC
-LIMIT 1;
--- backend/src/repository/postgres/query/configuracao/update_configuracao.sql
+++
@@ -1,9 +0,0 @@
-UPDATE cadpos.Configuracao
-SET
-	DiretorioSalvamento = %s,
-	AtualizadoEm = NOW()
-WHERE Id = %s
-RETURNING
-	Id AS id,
-	DiretorioSalvamento AS diretorio_salvamento,
-	AtualizadoEm AS atualizado_em;
